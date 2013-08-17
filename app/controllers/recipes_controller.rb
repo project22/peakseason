5628ab92a4a03ceaac97d30ca2381881f8bfb6fe
JP Berti <jpberti@yahoo.com>
2013-08-17 15:57:04 -0700

    none

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,5 @@
-
-
 class RecipesController < ApplicationController
   def local_listing
-  	@month = session[:month]
-    @region = session[:region]
-  	# @items = Item.all
-  	@items = Item.where(:season_start.lte => @month,  :season_end.gte => @month )
 
   	# get a list of recipes from an api for specific items that are in season.
   	# Think on this logic
@@ -16,14 +10,12 @@ class RecipesController < ApplicationController
   	@recipes = rec_loader.get_rec(ings)
   	puts @recipes
   	# I would like to have the model Rec return a json and convert it in this action.
-
   end
+
   def recipes_json
   	rec_loader = Rec.new
   	ings = params[:q]
   	@recipes = rec_loader.get_rec(ings)
   	puts @recipes
-
   end
-
 end
