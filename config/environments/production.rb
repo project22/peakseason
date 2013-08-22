b40352c74755aaf766425408c98ef40f599dae93
JP Berti <jpberti@yahoo.com>
2013-08-21 19:03:28 -0700

    change manifest file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,13 @@ PeakSeason::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w(
+"fontawesome-webfont.ttf",
+"fontawesome-webfont.eot",
+"fontawesome-webfont.svg",
+"fontawesome-webfont.woff")
+
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
